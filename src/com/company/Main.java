9f4207741181978da4fa24bb4778e044a4cf6f50
misha <shevel55505@gmail.com>
2020-06-24 09:42:15 +0300

    intitial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static java.lang.Thread.sleep;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setPreferredSize(new Dimension(600,300));
+        Container c = frame.getContentPane();
+        c.setLayout(new FlowLayout());
+        JButton butt = new JButton("butt");
+        JPanel pan = new JPanel();
+        pan.setPreferredSize(new Dimension(300, 100));
+        pan.setLayout(new BoxLayout(pan, BoxLayout.Y_AXIS));
+        JLabel lbl = new JLabel("Button is preesed");
+        lbl.setVisible(false);
+        pan.setBorder(BorderFactory.createLineBorder(Color.RED));
+        pan.add(lbl);
+        pan.add(butt);
+        frame.add(pan);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        try{sleep(1000);} catch(InterruptedException e){}
+        lbl.setVisible(true);
+
+
+
+    }
+}
